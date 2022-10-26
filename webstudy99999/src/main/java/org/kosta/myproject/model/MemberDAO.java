24fb13f92e1a11df5cbaa1644b18b3b499f8acd4
HAYOUNG LEE <hyl9410@gmail.com>
2022-10-26 17:26:39 +0900

    다시 받아주...

--- webstudy99999/src/main/java/org/kosta/myproject/model/MemberDAO.java
+++ webstudy99999/src/main/java/org/kosta/myproject/model/MemberDAO.java
@@ -1,5 +1,21 @@
 package org.kosta.myproject.model;
 
-public class MemberDAO {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private static MemberDAO instance=new MemberDAO();
+	private DataSource dataSource; 
+	private MemberDAO() {
+		dataSource=DataSourceManager.getInstance().getDataSource(); 
+	}
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	public  Connection getConnection() throws SQLException{
+		return dataSource.getConnection(); 
+	}
+	
 }
